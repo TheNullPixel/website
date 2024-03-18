8d83e549d5f9995c0db34c173dc16465dc03d7b9
null-pixel <nullpixeldev@proton.me>
2024-03-18 20:50:26 +0000

    scroll bar and deploy

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Raw HTML, CSS and JS",
     description: "A simple terminal website containing predefined commands.",
     image: "./project-images/terminal.png",
-    link: "https://github.com/null-pixel-dev/null-pixel-dev.github.io",
+    link: "https://github.com/null-pixel-dev/terminal-website",
     techStack: [
       {
         name: "HTML",
@@ -13,6 +13,31 @@ export const projects = [
       {
         name: "CSS",
         icon: "./icons/css3.svg",
+      },
+      {
+        name: "JavaScript",
+        icon: "./icons/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Portfolio Website",
+    subtitle: "React, HTML, TailwindCSS and JS",
+    description: "This website!",
+    image: "./project-images/portfolio.png",
+    link: "https://github.com/null-pixel-dev/null-pixel-dev.github.io",
+    techStack: [
+      {
+        name: "Reac",
+        icon: "./icons/react.svg",
+      },
+      {
+        name: "HTML",
+        icon: "./icons/html5.svg",
+      },
+      {
+        name: "TailwindCSS",
+        icon: "./icons/tailwindcss.svg",
       },
       {
         name: "JavaScript",
