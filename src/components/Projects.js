84de7914048c145b54b93d3ad5c1baa8035dfbbd
null-pixel-dev <nullpixeldev@proton.me>
2024-03-18 01:28:29 +0000

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,7 +27,7 @@ export default function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-cover object-top"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray bg-black opacity-0 hover:opacity-100">
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Terminal Website",
     subtitle: "Raw HTML, CSS and JS",
     description: "A simple terminal website containing predefined commands.",
-    image: "./terminal.png",
+    image: "./project-images/terminal.png",
     link: "https://github.com/null-pixel-dev/null-pixel-dev.github.io",
   },
 ];
